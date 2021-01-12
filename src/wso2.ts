b6368321a0513ce14fd972c300a2975dfe386a5b
Cecilia Woodward <ceci@salte.io>
2021-01-11 20:43:12 -0600

    chore: update dependencies

--- src/wso2.ts
+++ src/wso2.ts
@@ -5,15 +5,15 @@ export class WSO2 extends OpenIDProvider {
     super(config);
   }
 
-  public get name() {
+  public get name(): string {
     return 'wso2';
   }
 
-  public get login() {
+  public get login(): string {
     return this.url(`${this.config.url}/oauth2/authorize`);
   }
 
-  public get logout() {
+  public get logout(): string {
     // https://localhost:9443/oidc/logout?id_token_hint=<id_token>&post_logout_redirect_uri=<redirect URI>&state=<state>
     return this.url(`${this.config.url}/oidc/logout`, {
       id_token_hint: this.idToken.raw,
